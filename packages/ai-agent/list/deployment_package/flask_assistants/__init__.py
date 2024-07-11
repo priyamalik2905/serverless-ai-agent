47f0be591495eff012324aa5b0f2d66f1fca8124
Jdiv <thinquingquietly@gmail.com>
2024-07-11 14:14:30 -0400

    2nd real commit

--- packages/ai-agent/list/deployment_package/flask_assistants/__init__.py
+++
@@ -1,3 +0,0 @@
-from .extension import FlaskAssistants  # This imports the main class of the extension.
-
-__all__ = ['Assistant']
--- packages/ai-agent/list/deployment_package/list.py
+++
@@ -1,8 +0,0 @@
-import openai
-from flask_assistants import FlaskAssistants
-
-def main(event):
-      name = event.get("name", "stranger")
-      greeting = "Here's a list of " + name + "s!"
-      print(greeting)
-      return {"body": greeting}
